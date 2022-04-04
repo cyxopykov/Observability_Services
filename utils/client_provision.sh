84a850a704b9b4bd6284241bb898021014b747c1
Anatol Sukharukau <80684660+cyxopykov@users.noreply.github.com>
2022-04-04 20:45:13 +0300

    Delete utils directory

--- utils/client_provision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Client
-
-sudo yum -y install openldap-clients nss-pam-ldapd
-sudo authconfig --enableldap --enableldapauth --ldapserver=192.168.80.4 --ldapbasedn="dc=devopsldab,dc=com" --enablemkhomedir --update
-getent passwd 
